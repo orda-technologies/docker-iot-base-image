43ba8256ae5c433d1c3baf3bf1e79bbb28683d5d
Peter Juras <peter@orda-app.com>
2018-06-24 12:16:33 +0200

    Use circleci instead of travis

--- docker.sh
+++
@@ -1,8 +0,0 @@
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-REPO=orda/iot-base
-TAG=`if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then echo "latest"; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi`
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-docker push $REPO
